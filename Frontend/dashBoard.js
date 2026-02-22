d01880fd0e49231ae946b0c159e002df03bed0cc
Syed Ali <s_a577@txstate.edu>
2026-02-21 18:47:59 -0600

    added 403 and token check

--- Frontend/dashBoard.js
+++ Frontend/dashBoard.js
@@ -1,4 +1,8 @@
 document.getElementById("logout").addEventListener('click', (e) =>{
+    const token = localStorage.getItem('token');
+    if(!token){
+        window.location.replace('403.html');
+    }
     localStorage.clear()
     window.location.href ='loggedout.html';
 })
