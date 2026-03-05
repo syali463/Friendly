96d8aca9d60810f3e324176e76e102d41432c5d6
Syed Ali <s_a577@txstate.edu>
2026-03-05 12:35:13 -0600

    Styled dashboard and added modal for add subscription and javascript

--- Frontend/dashBoard.js
+++ Frontend/dashBoard.js
@@ -1,3 +1,15 @@
+const addBtn = document.getElementById("addSubscription")
+const modal = document.getElementById("modal")
+const closeBtn = document.getElementById("closeModal")
+
+addBtn.addEventListener('click', (e) => {
+    modal.style.display = 'flex';
+})
+
+closeBtn.addEventListener('click', (e) => {
+    modal.style.display = 'none';
+})
+
 document.getElementById("logout").addEventListener('click', (e) =>{
     const token = localStorage.getItem('token');
     if(!token){
