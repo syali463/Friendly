e59d5e569859e06f8fab1bc3840a443dfc4be163
Syed Ali <s_a577@txstate.edu>
2026-03-05 12:33:13 -0600

    Installed playwright and depencies and moved json packages to root folder.

---
+++ playwright.config.js
@@ -0,0 +1,95 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    headless : false,
+    launchOptions : {
+      slowMo : 2000,
+    },
+    trace: 'on',
+    screenshot : 'on',
+    video : "on-first-retry",
+    baseURL: 'http://localhost:5500',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  webServer: [
+    {
+    command: 'node Backend.js',
+    cwd: './backend',
+    url: 'http://localhost:3000/test',
+    reuseExistingServer: !process.env.CI,
+    stdout : 'pipe',
+  },
+  {
+    command: 'npx serve ./frontend -p 5500',
+    url: 'http://localhost:5500',
+    reuseExistingServer: !process.env.CI,
+    stdout:'pipe',
+    stderr:'pipe'
+  }
+],
+});
+
---
+++ tests/example.spec.js
@@ -0,0 +1,33 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:5500/loginPage');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Login/);
+});
+
+test('try login', async ({page}) =>{
+  await page.goto('http://localhost:5500/loginPage')
+
+  await page.getByPlaceholder('email').fill('syali463@gmail.com');
+  await page.getByPlaceholder('password').fill('Asif1234.');
+  await page.getByRole('button', {name: 'Login'}).click();
+
+  await page.pause();
+  await expect(page).toHaveTitle('Dashboard');
+
+})
+
+test('try wrong login', async ({page}) =>{
+  await page.goto('http://localhost:5500/loginPage')
+
+  await page.getByPlaceholder('email').fill('syal463@gmail.com');
+  await page.getByPlaceholder('password').fill('Asif1234.');
+  await page.getByRole('button', {name: 'Login'}).click();
+
+  await page.pause();
+  await expect(page.getByTestId('messageTest')).toHaveText('Invalid username or password.');
+
+})
