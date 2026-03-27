8dcab699ae31dcb9fd8c1d5ae43e610f09a07554
Syed Ali <s_a577@txstate.edu>
2026-03-27 13:54:57 -0500

    Database test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,33 +8,33 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Login/);
 });
 
-// test('google', async ({ page }) => {
-//   await page.goto('https://www.google.com');
+test('google', async ({ page }) => {
+  await page.goto('https://www.google.com');
 
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Google/);
-// });
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Google/);
+});
 
-// test('try login', async ({page}) =>{
-//   await page.goto('http://localhost:5500/loginPage')
+test('try login', async ({page}) =>{
+  await page.goto('http://localhost:5500/loginPage')
 
-//   await page.getByPlaceholder('email').fill('syali463@gmail.com');
-//   await page.getByPlaceholder('password').fill('Asif1234.');
-//   await page.getByRole('button', {name: 'Login'}).click();
+  await page.getByPlaceholder('email').fill('syali463@gmail.com');
+  await page.getByPlaceholder('password').fill('Asif1234.');
+  await page.getByRole('button', {name: 'Login'}).click();
 
-//   await page.pause();
-//   await expect(page).toHaveTitle('Dashboard');
+  await page.pause();
+  await expect(page).toHaveTitle('Dashboard');
 
-// })
+})
 
-// test('try wrong login', async ({page}) =>{
-//   await page.goto('http://localhost:5500/loginPage')
+test('try wrong login', async ({page}) =>{
+  await page.goto('http://localhost:5500/loginPage')
 
-//   await page.getByPlaceholder('email').fill('syal463@gmail.com');
-//   await page.getByPlaceholder('password').fill('Asif1234.');
-//   await page.getByRole('button', {name: 'Login'}).click();
+  await page.getByPlaceholder('email').fill('syal463@gmail.com');
+  await page.getByPlaceholder('password').fill('Asif1234.');
+  await page.getByRole('button', {name: 'Login'}).click();
 
-//   await page.pause();
-//   await expect(page.getByTestId('messageTest')).toHaveText('Invalid username or password.');
+  await page.pause();
+  await expect(page.getByTestId('messageTest')).toHaveText('Invalid username or password.');
 
-// })
+})
